cdb6ec8af2651ca8c406893f632b8991cbf2be10
ariele-fatima <ariele_afd@yahoo.com.br>
2020-11-28 22:56:04 -0300

    Alterando para Template Engine e criando as rotas

---
+++ src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633", 
+        lng: "-49.6455874",
+        name: "Lar das Meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se enconter em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1540479859555-17af45c78602?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+            "https://images.unsplash.com/photo-1571210862729-78a52d3779a2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+
+    },
+    {
+        id: 2,
+        lat: "-27.222633", 
+        lng: "-49.6555874",
+        name: "Lar dos Meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se enconter em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1517164850305-99a3e65bb47e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+            "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+
+    }
+]
